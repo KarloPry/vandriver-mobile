045423bcc93a42c6863ed387ebe3e90511466d34
KarloPry <kpaz30@alumnos.uaq.mx>
2024-04-19 23:37:18 -0600

    Starting Screen

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+  Pressable,
+} from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("./assets/images/logo-letters.png")}
+          style={styles.imageLogoText}
+          width={500}
+          height={500}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.textInput}>Correo electrónico</Text>
+        <TextInput
+          style={styles.input}
+          keyboardType="email-address"
+          placeholder="Correo electrónico"
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.textInput}>Contraseña</Text>
+        <TextInput
+          style={styles.input}
+          keyboardType="email-address"
+          placeholder="Contraseña"
+        />
+      </View>
+      <View style={{display: "flex", flexDirection: "row", width: "100%", justifyContent: "center" ,marginVertical: 20}}>
+      <Pressable style={{backgroundColor:"#1C385F", borderRadius: 3, paddingVertical: 4, paddingHorizontal: 20, alignContent: "center", alignItems: "center"}}>
+        <Text style={{color: "white", fontSize: 22}}>Iniciar sesión</Text>
+      </Pressable>
+      </View>
     </View>
   );
 }
@@ -13,8 +47,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "left",
+    justifyContent: "center",
+  },
+  textInput: {
+    paddingHorizontal: 9,
+    paddingVertical: 10,
+  },
+  input: {
+    display: "flex",
+    backgroundColor: "#f4f4f4",
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    textAlign: "left",
+  },
+  inputContainer: {
+    paddingHorizontal: 30,
+  },
+  imageContainer: {
+    alignItems: "center",
   },
 });
