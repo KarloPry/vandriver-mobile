c627a349731bf043da552ced7a4c0fbed7ec94bb
KarloPry <kpaz30@alumnos.uaq.mx>
2024-04-20 02:07:34 -0600

    Router implemented

--- App.js
+++ App.js
@@ -1,72 +1,15 @@
-import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  Image,
-  Pressable,
-} from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+const Stack = createStackNavigator();
+import LoginRoute from "./app/auth/login/route";
+import SignupRoute from "./app/auth/signup/route";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <View style={styles.imageContainer}>
-        <Image
-          source={require("./assets/images/logo-letters.png")}
-          style={styles.imageLogoText}
-          width={500}
-          height={500}
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.textInput}>Correo electrónico</Text>
-        <TextInput
-          style={styles.input}
-          keyboardType="email-address"
-          placeholder="Correo electrónico"
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.textInput}>Contraseña</Text>
-        <TextInput
-          style={styles.input}
-          keyboardType="email-address"
-          placeholder="Contraseña"
-        />
-      </View>
-      <View style={{display: "flex", flexDirection: "row", width: "100%", justifyContent: "center" ,marginVertical: 20}}>
-      <Pressable style={{backgroundColor:"#1C385F", borderRadius: 3, paddingVertical: 4, paddingHorizontal: 20, alignContent: "center", alignItems: "center"}}>
-        <Text style={{color: "white", fontSize: 22}}>Iniciar sesión</Text>
-      </Pressable>
-      </View>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginRoute} />
+        <Stack.Screen name="Signup" component={SignupRoute} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "left",
-    justifyContent: "center",
-  },
-  textInput: {
-    paddingHorizontal: 9,
-    paddingVertical: 10,
-  },
-  input: {
-    display: "flex",
-    backgroundColor: "#f4f4f4",
-    paddingVertical: 12,
-    paddingHorizontal: 20,
-    borderRadius: 8,
-    textAlign: "left",
-  },
-  inputContainer: {
-    paddingHorizontal: 30,
-  },
-  imageContainer: {
-    alignItems: "center",
-  },
-});
---
+++ app/auth/login/route.js
@@ -0,0 +1,89 @@
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+export default function LoginRoute({navigation}) {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("../../../assets/images/logo-letters.png")}
+          style={styles.imageLogoText}
+          width={500}
+          height={500}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.textInput}>Correo electrónico</Text>
+        <TextInput
+          style={styles.input}
+          keyboardType="email-address"
+          placeholder="Correo electrónico"
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.textInput}>Contraseña</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Contraseña"
+          secureTextEntry={true}
+        />
+      </View>
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          width: "100%",
+          justifyContent: "center",
+          marginVertical: 20,
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#1C385F",
+            borderRadius: 3,
+            paddingVertical: 4,
+            paddingHorizontal: 20,
+            alignContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ color: "white", fontSize: 22 }}>Iniciar sesión</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "left",
+    justifyContent: "center",
+  },
+  textInput: {
+    paddingHorizontal: 9,
+    paddingVertical: 10,
+  },
+  input: {
+    display: "flex",
+    backgroundColor: "#f4f4f4",
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    textAlign: "left",
+  },
+  inputContainer: {
+    paddingHorizontal: 30,
+  },
+  imageContainer: {
+    alignItems: "center",
+  },
+});
